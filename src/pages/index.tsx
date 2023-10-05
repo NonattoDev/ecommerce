1c16e6b51cbb5990993f6cab62313621d826a203
Robson Nonato <robsonnonatoiii@gmail.com>
2023-10-04 22:33:06 -0300

    Alteracoes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import ProdutoCard from "@/components/Produto/produto";
 import Container from "react-bootstrap/Container";
@@ -5,8 +6,25 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Carrossel from "@/components/Carrossel/carrossel";
 import NavbarSite from "@/components/Navbar/Navbar";
+import axios from "axios";
+import axiosCliente from "@/services/axiosCliente";
 
 export default function Home() {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    obterProdutosPaginados(1); // Carrega a página inicial ao carregar o componente
+  }, []);
+
+  const obterProdutosPaginados = async (pagina) => {
+    try {
+      const resposta = await axiosCliente.get(`/produtos?pagina=${pagina}&itensPorPagina=8`);
+      setProdutos(resposta.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -16,22 +34,11 @@ export default function Home() {
       <NavbarSite />
       <Container>
         <Row xs={1} md={2} lg={3} xl={4} className="g-4">
-          <Col>
-            <ProdutoCard nome="Camisa do Brasil" preco={40} descricao="Camisa Linda" />
-            <ProdutoCard nome="Camisa do Brasil" preco={40} descricao="Camisa Linda" />
-          </Col>
-          <Col>
-            <ProdutoCard nome="Camisa do Brasil" preco={40} descricao="Camisa Linda" />
-            <ProdutoCard nome="Camisa do Brasil" preco={40} descricao="Camisa Linda" />
-          </Col>
-          <Col>
-            <ProdutoCard nome="Camisa do Brasil" preco={40} descricao="Camisa Linda" />
-            <ProdutoCard nome="Camisa do Brasil" preco={40} descricao="Camisa Linda" />
-          </Col>
-          <Col>
-            <ProdutoCard nome="Camisa do Brasil" preco={40} descricao="Camisa Linda" />
-            <ProdutoCard nome="Camisa do Brasil" preco={40} descricao="Camisa Linda" />
-          </Col>
+          {produtos.map((produto) => (
+            <Col key={produto.id}>
+              <ProdutoCard nome={produto.nome} preco={produto.preco} descricao={produto.descricao} />
+            </Col>
+          ))}
         </Row>
       </Container>
     </>
