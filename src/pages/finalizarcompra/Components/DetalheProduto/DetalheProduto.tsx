6baed524ba7c3af8455af7665cd14efc870dd892
Robson Nonato <108809490+NonattoDev@users.noreply.github.com>
2023-11-07 10:04:49 -0300

    Corrigindo Tipagens

--- src/pages/finalizarcompra/Components/DetalheProduto/DetalheProduto.tsx
+++ src/pages/finalizarcompra/Components/DetalheProduto/DetalheProduto.tsx
@@ -5,23 +5,29 @@ import styles from "./DetalheProduto.module.css";
 import { useCarrinhoContext } from "@/context/CarrinhoContext";
 
 const DetalheProduto: React.FC<{ produto: Produto }> = ({ produto }) => {
+
+  if (!produto) {
+    // Se o produto não estiver disponível, retorne um placeholder ou null
+    return <div>Produto não encontrado</div>;
+  }
+
   const { handleRemoverProduto, handleAtualizarQuantidadeProduto } = useCarrinhoContext();
 
   // Adicione uma verificação para garantir que o produto e a propriedade Caminho existam
   const imagePath = produto?.Caminho ? `/fotosProdutos/${produto.Caminho}` : '/fotosProdutos/erro/semProduto.png'; // Caminho padrão ou imagem padrão
 
   return (
-    <Card key={produto.CodPro} className={styles.cardContainer}>
+    <Card key={produto?.CodPro} className={styles.cardContainer}>
       <Card.Body>
         <div className={styles.cardProdutoDetalheContainer}>
           <div className={styles.produtoInfo}>
             <div>
               {/* Use a variável imagePath para o atributo src da imagem */}
-              <Image src={imagePath} alt={produto.Produto} width={100} height={100} />
+              <Image src={imagePath} alt={produto?.Produto} width={100} height={100} />
             </div>
             <div>
-              <h5>{produto.Produto}</h5>
-              <p className={styles.botaoExcluirItem} onClick={() => handleRemoverProduto(produto.CodPro)}>
+              <h5>{produto?.Produto}</h5>
+              <p className={styles.botaoExcluirItem} onClick={() => handleRemoverProduto(produto?.CodPro)}>
                 Excluir
               </p>
             </div>
@@ -31,21 +37,21 @@ const DetalheProduto: React.FC<{ produto: Produto }> = ({ produto }) => {
               <Form.Control
                 name="quantidadeProduto"
                 type="number"
-                value={produto.Quantidade > produto.Estoque ? produto.Estoque : produto.Quantidade || 0}
+                value={produto?.Quantidade > produto?.Estoque ? produto?.Estoque : produto?.Quantidade || 0}
                 onChange={(e) => {
                   const quantidade = e.target.value === "" ? 1 : parseInt(e.target.value);
-                  const valorMaximo = produto.Estoque;
+                  const valorMaximo = produto?.Estoque;
                   const novaQuantidade = quantidade > valorMaximo ? valorMaximo : quantidade;
-                  handleAtualizarQuantidadeProduto(produto.CodPro, novaQuantidade);
+                  handleAtualizarQuantidadeProduto(produto?.CodPro, novaQuantidade);
                 }}
                 className={styles.inputQuantidade}
                 min={1}
-                max={produto.Estoque}
+                max={produto?.Estoque}
               />
             </Form>
           </div>
           <div className={styles.produtoPreco}>
-            <p>R$: {produto.Preco1.toLocaleString("pt-br")}</p>
+            <p>R$: {produto?.Preco1.toLocaleString("pt-br")}</p>
           </div>
         </div>
       </Card.Body>
