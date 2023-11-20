f6ac816a6a40f1333c9bcb408578c64895be33d4
Robson Nonato <108809490+NonattoDev@users.noreply.github.com>
2023-11-20 15:03:50 -0300

    Excessao para Webhook em token protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,11 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
 
+  // Exceção para a rota /api/vendas/notificacao
+  if (request.nextUrl.pathname === "/api/vendas/notificacao") {
+    return NextResponse.next();
+  }
+
   if (request.nextUrl.pathname.startsWith("/api/admin/")) {
     if (token && !token.admin) {
       return new Response("Não autorizado", { status: 401 });
