007daee3c813f72db9dec29aba3adda44d79dcc0
Robson Nonato <108809490+NonattoDev@users.noreply.github.com>
2023-11-27 15:06:30 -0300

    Correcao Favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
 import { CarrinhoProvider } from "@/context/CarrinhoContext";
 import Footer from "@/components/Footer/footer";
 import Header from "@/components/Header/header";
@@ -14,6 +15,10 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   return (
     <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>
+        <Head>
+          <title>E-commerce Soft Line</title>
+          <link rel="shortcut icon" href={`${process.env.NEXT_PUBLIC_FAVICON}`} type="image/x-icon" />
+        </Head>
         <CarrinhoProvider>
           <Header />
           <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head>
-        <title>E-commerce Soft Line</title>
-        <link rel="shortcut icon" href={`${process.env.NEXT_PUBLIC_FAVICON}`} type="image/x-icon" />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
