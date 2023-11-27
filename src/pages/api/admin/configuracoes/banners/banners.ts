c75dcef330d2ef3fbe78f5977064a46a3d321000
Robson Nonato <108809490+NonattoDev@users.noreply.github.com>
2023-11-27 17:07:26 -0300

    Atualizando configuracoes no portal

--- src/pages/api/admin/configuracoes/banners/banners.ts
+++ src/pages/api/admin/configuracoes/banners/banners.ts
@@ -3,21 +3,30 @@ import AWS from "aws-sdk";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    // Lógica para lidar com a requisição GET aqui
+    // Verifica se o nome do bucket está definido
+    const bucketName = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME;
+    if (!bucketName) {
+      return res.status(500).json({ message: "Nome do bucket não configurado" });
+    }
+
     const s3 = new AWS.S3({
       accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
-      endpoint: process.env.NEXT_PUBLIC_AWS_S3_ENDPOINT, // Exemplo de endpoint do Backblaze
-      s3ForcePathStyle: true, // necessário com Backblaze
+      endpoint: process.env.NEXT_PUBLIC_AWS_S3_ENDPOINT,
+      s3ForcePathStyle: true,
     });
 
     const params = {
-      Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME,
-      Prefix: "banners/", // Pasta Banners
+      Bucket: bucketName,
+      Prefix: "banners/",
     };
 
-    const data = await s3.listObjectsV2(params).promise();
-    return res.status(200).json(data.Contents?.map((file) => file.Key) || []);
+    try {
+      const data = await s3.listObjectsV2(params).promise();
+      return res.status(200).json(data.Contents?.map((file) => file.Key) || []);
+    } catch (error: any) {
+      return res.status(500).json({ message: "Erro ao listar objetos do S3", error: error.message });
+    }
   } else {
     res.status(405).json({ message: "Método não permitido" });
   }
