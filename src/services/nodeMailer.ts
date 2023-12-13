e85f70c903a739c49adfce47ba4ca9998ebe8a50
Robson Nonato <robsonnonatoiii@gmail.com>
2023-12-13 13:54:01 -0300

    Alteracao nodemailer

--- src/services/nodeMailer.ts
+++ src/services/nodeMailer.ts
@@ -2,7 +2,9 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
   port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_LOGIN,
     pass: process.env.GMAIL_PSWD,
