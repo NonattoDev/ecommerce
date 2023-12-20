8686668114b5c44c9fd5cfe91c8296fe7cfcfa37
Robson Nonato <108809490+NonattoDev@users.noreply.github.com>
2023-12-20 09:51:05 -0300

    nitro

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,10 @@
-import knex from "knex";
-const dbConnectionString = `Data Source=189.89.178.14,65533;Network Library=DBMSSOCN;
-Initial Catalog=IMPORT;User ID=Soft Line;Password=4321;`;
-
 const db = knex({
-  client: "mssql",
-  connection: dbConnectionString,
+  client: 'mssql',
+  connection: {
+    host : '189.89.178.14',
+    user : 'Soft Line',
+    password : '4321',
+    database : 'IMPORT',
+    port: 65533
+  }
 });
-
-export default db;
